03fbfbac87c35c9c4eab1c752dd5577a2aa45af3
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2023-10-24 20:52:36 +0700

    refactor: move to modules dir

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,6 @@
-import { Icon } from "@iconify/react";
-import Head from "next/head";
-import { Button } from "~/components/ui/button";
+import type { NextPage } from "next";
+import { HomePage } from "~/modules/home/HomePage";
 
-export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Chiral</title>
-        <meta name="description" content="Automate your product backlogs" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className="flex min-h-screen w-screen flex-col items-center justify-center p-8">
-        <div className="flex flex-col items-center text-center">
-          <h1 className="text-5xl font-bold">Automate your product backlogs</h1>
+const Home: NextPage = () => <HomePage />;
 
-          <p className="text-bold my-6 max-w-xl text-lg text-slate-200 md:text-xl">
-            Chiral generates{" "}
-            <span className="text-primary font-bold">clear</span> and{" "}
-            <span className="text-primary font-bold">actionable</span> tickets
-            from your PRDs and maximize your team&apos;s productivity.
-          </p>
-
-          <div className="flex flex-col items-center gap-4">
-            <Button className="bg-primary-dark flex flex-row place-content-center gap-2 px-10 py-7 text-base">
-              <Icon icon="mingcute:linear-fill" fontSize={24} />
-              Sign in with Linear
-            </Button>
-          </div>
-        </div>
-      </main>
-    </>
-  );
-}
+export default Home;
