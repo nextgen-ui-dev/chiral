cf5e7fab7921c6fbef59990042dfddc656400f77
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2023-10-27 08:55:44 +0700

    feat: add profile and workspace popover

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,15 +1,35 @@
 import React from "react";
+import { Button } from "~/components/ui/button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "~/components/ui/popover";
 import { api } from "~/utils/api";
 
 export const DashboardLayout: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   const { data: user, isLoading } = api.user.getCurrentUser.useQuery();
+  const { data: workspace, isLoading: workspaceLoading } =
+    api.workspace.getCurrentWorkspace.useQuery();
+
   return (
-    !isLoading && (
+    !isLoading &&
+    !workspaceLoading && (
       <div className="flex min-h-screen w-full flex-row">
         <nav className="min-h-screen min-w-[20rem] max-w-xs overflow-x-clip border-r-[1px] border-solid border-primary-dark p-4">
-          {user?.name}
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button size="sm" className="px-2" variant="ghost">
+                {workspace?.name}
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="ml-4 mt-2 p-2">
+              <p className="text-sm text-slate-300">{user?.name}</p>
+              <p className="text-xs text-slate-300">{user?.email}</p>
+            </PopoverContent>
+          </Popover>
         </nav>
         <main className="w-full">{children}</main>
       </div>
--- src/server/api/routers/workspace.ts
+++ src/server/api/routers/workspace.ts
@@ -1,41 +1,38 @@
-import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 import { workspaces } from "~/server/db/schema";
 import { and, eq, sql } from "drizzle-orm";
 import { TRPCError } from "@trpc/server";
 
 export const workspaceRouter = createTRPCRouter({
-  getCurrentWorkspace: protectedProcedure
-    .input(z.object({ workspaceId: z.string() }))
-    .query(async ({ ctx, input }) => {
-      const { workspaceId } = input;
-      const args = workspaceId.split(":");
-      if (args.length !== 2) {
-        throw new TRPCError({
-          code: "NOT_FOUND",
-          message: "Workspace not found",
-        });
-      }
+  getCurrentWorkspace: protectedProcedure.query(async ({ ctx }) => {
+    const workspaceId = ctx.session.workspace_id;
+    const args = workspaceId.split(":");
+    if (args.length !== 2) {
+      throw new TRPCError({
+        code: "NOT_FOUND",
+        message: "Workspace not found",
+      });
+    }
 
-      const res = await ctx.db
-        .select()
-        .from(workspaces)
-        .where(
-          and(
-            sql`${workspaces.providerId} = ${args[0]}`,
-            eq(workspaces.providerWorkspaceId, args[1]!),
-            eq(workspaces.accountId, ctx.session.account_id),
-          ),
-        )
-        .limit(1);
+    const res = await ctx.db
+      .select()
+      .from(workspaces)
+      .where(
+        and(
+          sql`${workspaces.providerId} = ${args[0]}`,
+          eq(workspaces.providerWorkspaceId, args[1]!),
+          eq(workspaces.accountId, ctx.session.account_id),
+        ),
+      )
+      .limit(1);
 
-      if (res.length < 1) {
-        throw new TRPCError({
-          code: "NOT_FOUND",
-          message: "Workspace not found",
-        });
-      }
+    if (res.length < 1) {
+      throw new TRPCError({
+        code: "NOT_FOUND",
+        message: "Workspace not found",
+      });
+    }
 
-      return res[0]!;
-    }),
+    return res[0]!;
+  }),
 });
