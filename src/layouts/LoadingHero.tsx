0aa833556b9a499228e6aa7c2291283e4f08ffbe
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2023-10-27 20:49:22 +0700

    feat: use favicon for loading hero

--- src/layouts/LoadingHero.tsx
+++ src/layouts/LoadingHero.tsx
@@ -1,9 +1,9 @@
-import { Loader2 } from "lucide-react";
+import Image from "next/image";
 
 export const LoadingHero = () => (
   <main className="flex min-h-screen w-full flex-col items-center justify-center p-8">
     <div className="flex animate-pulse flex-col items-center text-center">
-      <Loader2 className="animate-spin font-bold text-primary" size={40} />
+      <Image src="/favicon.ico" alt="Chiral Icon" width={60} height={60} />
       <p className="mt-4 text-xl text-slate-300">Loading...</p>
     </div>
   </main>
