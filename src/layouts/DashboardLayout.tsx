b268884e3a8dd13f6108a7a5cc886aa9f6740e0d
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2023-10-28 12:42:09 +0700

    feat: add loading hero in dashboard

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -24,6 +24,7 @@ import {
   PopoverTrigger,
 } from "~/components/ui/popover";
 import { api } from "~/utils/api";
+import { LoadingHero } from "./LoadingHero";
 
 export const DashboardLayout: React.FC<{ children: React.ReactNode }> = ({
   children,
@@ -51,179 +52,176 @@ export const DashboardLayout: React.FC<{ children: React.ReactNode }> = ({
     router.reload();
   };
 
-  return (
-    !isLoading &&
-    !workspaceLoading &&
-    !sessionLoading &&
-    !teamsLoading && (
-      <div className="flex min-h-screen w-full flex-row">
-        <nav className="min-h-screen min-w-[20rem] max-w-xs overflow-x-clip border-r-[1px] border-solid border-primary-dark p-4">
-          <Popover>
-            <PopoverTrigger asChild>
-              <Button
-                size="sm"
-                className="flex flex-row gap-2 px-2 hover:bg-primary-dark"
-                variant="ghost"
-              >
-                <Avatar className="h-7 w-7">
-                  <AvatarImage
-                    src={user?.avatar_url ?? ""}
-                    alt={user?.name + "avatar"}
-                  />
-                  <AvatarFallback>
-                    {user?.name
-                      ?.split(" ")
-                      .map((n) => n[0])
-                      .join("")}
-                  </AvatarFallback>
-                </Avatar>
-                {workspace?.name}
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent className="ml-4 mt-2 p-2">
-              <p className="text-sm text-slate-300">{user?.name}</p>
-              <p className="text-xs text-slate-300">{user?.email}</p>
-              <hr className="my-2 border-primary" />
-              <p className="pl-1 text-xs text-slate-300">Workspaces</p>
-              {!workspacesLoading && (
-                <ul>
-                  {data?.map((ws) => {
-                    const now = Date.now();
-                    const expired =
-                      ws.sessions === null || ws.sessions.activeExpires < now;
-                    const workspaceId =
-                      ws.workspaces.providerId +
-                      ":" +
-                      ws.workspaces.providerWorkspaceId;
-                    const activeWsId =
-                      workspace?.providerId +
-                      ":" +
-                      workspace?.providerWorkspaceId;
+  return isLoading || workspaceLoading || sessionLoading || teamsLoading ? (
+    <LoadingHero />
+  ) : (
+    <div className="flex min-h-screen w-full flex-row">
+      <nav className="min-h-screen min-w-[20rem] max-w-xs overflow-x-clip border-r-[1px] border-solid border-primary-dark p-4">
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button
+              size="sm"
+              className="flex flex-row gap-2 px-2 hover:bg-primary-dark"
+              variant="ghost"
+            >
+              <Avatar className="h-7 w-7">
+                <AvatarImage
+                  src={user?.avatar_url ?? ""}
+                  alt={user?.name + "avatar"}
+                />
+                <AvatarFallback>
+                  {user?.name
+                    ?.split(" ")
+                    .map((n) => n[0])
+                    .join("")}
+                </AvatarFallback>
+              </Avatar>
+              {workspace?.name}
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="ml-4 mt-2 p-2">
+            <p className="text-sm text-slate-300">{user?.name}</p>
+            <p className="text-xs text-slate-300">{user?.email}</p>
+            <hr className="my-2 border-primary" />
+            <p className="pl-1 text-xs text-slate-300">Workspaces</p>
+            {!workspacesLoading && (
+              <ul>
+                {data?.map((ws) => {
+                  const now = Date.now();
+                  const expired =
+                    ws.sessions === null || ws.sessions.activeExpires < now;
+                  const workspaceId =
+                    ws.workspaces.providerId +
+                    ":" +
+                    ws.workspaces.providerWorkspaceId;
+                  const activeWsId =
+                    workspace?.providerId +
+                    ":" +
+                    workspace?.providerWorkspaceId;
 
-                    return expired ? (
-                      <a
+                  return expired ? (
+                    <a
+                      key={workspaceId}
+                      href={
+                        "/api/auth/login/" +
+                        ws.workspaces.providerId +
+                        "?prompt=consent"
+                      }
+                    >
+                      <li
                         key={workspaceId}
-                        href={
-                          "/api/auth/login/" +
-                          ws.workspaces.providerId +
-                          "?prompt=consent"
-                        }
+                        className="flex flex-row items-center justify-between rounded-md px-2 py-1 text-sm hover:bg-primary"
                       >
-                        <li
-                          key={workspaceId}
-                          className="flex flex-row items-center justify-between rounded-md px-2 py-1 text-sm hover:bg-primary"
-                        >
-                          <p>{ws.workspaces.name}</p>
-                          {expired && (
-                            <p className="text-xs">Session timed out</p>
-                          )}
-                        </li>
-                      </a>
-                    ) : (
-                      <li key={workspaceId}>
-                        <button
-                          onClick={() =>
-                            void setWorkspaceSession(
-                              workspaceId,
-                              sessionData!.session!.id,
-                            )
-                          }
-                          className="flex w-full flex-row items-center justify-between rounded-md px-2 py-1 text-sm hover:bg-primary"
-                        >
-                          <p>{ws.workspaces.name}</p>
-                          {workspaceId === activeWsId && <Check size={16} />}
-                          {expired && (
-                            <p className="text-xs">Session timed out</p>
-                          )}
-                        </button>
+                        <p>{ws.workspaces.name}</p>
+                        {expired && (
+                          <p className="text-xs">Session timed out</p>
+                        )}
                       </li>
-                    );
-                  })}
-                </ul>
-              )}
-              <hr className="my-2 border-primary" />
-              <p className="pl-1 text-xs text-slate-300">Manage Workspace</p>
-              <Link href="/api/auth/login/linear?prompt=consent">
-                <button
-                  type="submit"
-                  className="flex w-full flex-row items-center gap-2 rounded-md px-2 py-1 text-sm hover:bg-primary"
-                >
-                  <Icon icon="mingcute:linear-fill" fontSize={16} />
-                  <p>Add Linear Workspace</p>
-                </button>
-              </Link>
-              <hr className="my-2 border-primary" />
-              <form action="/api/auth/logout" method="POST">
-                <button
-                  type="submit"
-                  className="flex w-full flex-row items-center gap-2 rounded-md px-2 py-1 text-sm text-red-600 hover:bg-primary"
-                >
-                  <LogOut size={16} />
-                  <p>Sign Out</p>
-                </button>
-              </form>
-            </PopoverContent>
-          </Popover>
-          <div className="scroll-m- max-h-full w-full overflow-y-auto">
-            <p className="mb-2 mt-4 pl-1 text-xs text-slate-300">My Teams</p>
-            <ul>
-              {teamsData?.teams.map((team) => {
-                return (
-                  <li key={team.id}>
-                    <Collapsible>
-                      <CollapsibleTrigger asChild>
-                        <Button
-                          variant="ghost"
-                          size="sm"
-                          className="flex h-auto w-full flex-row items-center justify-between p-2 hover:bg-primary"
-                        >
-                          <p className="text-sm font-semibold">{team.name}</p>
-                          <ChevronsUpDown size={16} />
-                        </Button>
-                      </CollapsibleTrigger>
-                      <CollapsibleContent className="text-sm">
-                        <ul className="ml-2 border-l-2 border-l-primary-dark pl-2">
-                          <li>
-                            <Button
-                              variant="ghost"
-                              size="sm"
-                              className="flex h-auto w-full flex-row items-center justify-start gap-2 p-2 hover:bg-primary"
-                            >
-                              <Ticket size={16} />
-                              <p className="font-semibold">Issues</p>
-                            </Button>
-                          </li>
-                          <li>
-                            <Button
-                              variant="ghost"
-                              size="sm"
-                              className="flex h-auto w-full flex-row items-center justify-start gap-2 p-2 hover:bg-primary"
-                            >
-                              <Briefcase size={16} />
-                              <p className="font-semibold">Projects</p>
-                            </Button>
-                          </li>
-                          <li>
-                            <Button
-                              variant="ghost"
-                              size="sm"
-                              className="flex h-auto w-full flex-row items-center justify-start gap-2 p-2 hover:bg-primary"
-                            >
-                              <Files size={16} />
-                              <p className="font-semibold">Documents</p>
-                            </Button>
-                          </li>
-                        </ul>
-                      </CollapsibleContent>
-                    </Collapsible>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        </nav>
-        <main className="w-full">{children}</main>
-      </div>
-    )
+                    </a>
+                  ) : (
+                    <li key={workspaceId}>
+                      <button
+                        onClick={() =>
+                          void setWorkspaceSession(
+                            workspaceId,
+                            sessionData!.session!.id,
+                          )
+                        }
+                        className="flex w-full flex-row items-center justify-between rounded-md px-2 py-1 text-sm hover:bg-primary"
+                      >
+                        <p>{ws.workspaces.name}</p>
+                        {workspaceId === activeWsId && <Check size={16} />}
+                        {expired && (
+                          <p className="text-xs">Session timed out</p>
+                        )}
+                      </button>
+                    </li>
+                  );
+                })}
+              </ul>
+            )}
+            <hr className="my-2 border-primary" />
+            <p className="pl-1 text-xs text-slate-300">Manage Workspace</p>
+            <Link href="/api/auth/login/linear?prompt=consent">
+              <button
+                type="submit"
+                className="flex w-full flex-row items-center gap-2 rounded-md px-2 py-1 text-sm hover:bg-primary"
+              >
+                <Icon icon="mingcute:linear-fill" fontSize={16} />
+                <p>Add Linear Workspace</p>
+              </button>
+            </Link>
+            <hr className="my-2 border-primary" />
+            <form action="/api/auth/logout" method="POST">
+              <button
+                type="submit"
+                className="flex w-full flex-row items-center gap-2 rounded-md px-2 py-1 text-sm text-red-600 hover:bg-primary"
+              >
+                <LogOut size={16} />
+                <p>Sign Out</p>
+              </button>
+            </form>
+          </PopoverContent>
+        </Popover>
+        <div className="scroll-m- max-h-full w-full overflow-y-auto">
+          <p className="mb-2 mt-4 pl-1 text-xs text-slate-300">My Teams</p>
+          <ul>
+            {teamsData?.teams.map((team) => {
+              return (
+                <li key={team.id}>
+                  <Collapsible>
+                    <CollapsibleTrigger asChild>
+                      <Button
+                        variant="ghost"
+                        size="sm"
+                        className="flex h-auto w-full flex-row items-center justify-between p-2 hover:bg-primary"
+                      >
+                        <p className="text-sm font-semibold">{team.name}</p>
+                        <ChevronsUpDown size={16} />
+                      </Button>
+                    </CollapsibleTrigger>
+                    <CollapsibleContent className="text-sm">
+                      <ul className="ml-2 border-l-2 border-l-primary-dark pl-2">
+                        <li>
+                          <Button
+                            variant="ghost"
+                            size="sm"
+                            className="flex h-auto w-full flex-row items-center justify-start gap-2 p-2 hover:bg-primary"
+                          >
+                            <Ticket size={16} />
+                            <p className="font-semibold">Issues</p>
+                          </Button>
+                        </li>
+                        <li>
+                          <Button
+                            variant="ghost"
+                            size="sm"
+                            className="flex h-auto w-full flex-row items-center justify-start gap-2 p-2 hover:bg-primary"
+                          >
+                            <Briefcase size={16} />
+                            <p className="font-semibold">Projects</p>
+                          </Button>
+                        </li>
+                        <li>
+                          <Button
+                            variant="ghost"
+                            size="sm"
+                            className="flex h-auto w-full flex-row items-center justify-start gap-2 p-2 hover:bg-primary"
+                          >
+                            <Files size={16} />
+                            <p className="font-semibold">Documents</p>
+                          </Button>
+                        </li>
+                      </ul>
+                    </CollapsibleContent>
+                  </Collapsible>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </nav>
+      <main className="w-full">{children}</main>
+    </div>
   );
 };
