4e4906fc8b0c33df36d9229074c1d502abb01324
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2023-10-31 01:01:42 +0700

    feat: use remark gfm plugin

--- src/modules/workspace/components/MarkdownDocumentView.tsx
+++ src/modules/workspace/components/MarkdownDocumentView.tsx
@@ -1,10 +1,12 @@
 import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 export const MarkdownDocumentView: React.FC<{ markdown?: string }> = ({
   markdown,
 }) => (
   <Markdown
     className="flex flex-col gap-y-6 overflow-y-auto px-6 py-4"
+    remarkPlugins={[remarkGfm]}
     components={{
       h1: (props) => <h1 className="text-4xl font-bold">{props.children}</h1>,
       h2: (props) => <h2 className="text-2xl font-bold">{props.children}</h2>,
