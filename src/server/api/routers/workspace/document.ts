c93227d1bcc31f01cf4a043b266e21576bb02817
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2023-11-01 03:55:58 +0700

    feat: create embedding logic

--- src/server/api/routers/workspace/document.ts
+++ src/server/api/routers/workspace/document.ts
@@ -3,16 +3,32 @@ import { createTRPCRouter, protectedProcedure } from "../../trpc";
 import { documentMessages, documents } from "~/server/db/schema";
 import { and, asc, eq, exists } from "drizzle-orm";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { HuggingFaceTransformersEmbeddings } from "langchain/embeddings/hf_transformers";
 import { ulid } from "~/lib/ulid";
 
-const markdownSplitter =
-  RecursiveCharacterTextSplitter.fromLanguage("markdown");
+const markdownSplitter = RecursiveCharacterTextSplitter.fromLanguage(
+  "markdown",
+  {
+    chunkSize: 250,
+    chunkOverlap: 0,
+  },
+);
+
+const embeddingModel = new HuggingFaceTransformersEmbeddings({
+  modelName: "Xenova/bge-large-en-v1.5",
+  maxConcurrency: 10,
+});
 
 export const documentRouter = createTRPCRouter({
   saveMarkdownEmbeddings: protectedProcedure
     .input(z.object({ markdown: z.string() }))
     .mutation(async ({ ctx, input: { markdown } }) => {
       const documents = await markdownSplitter.createDocuments([markdown]);
+      const embeddings = await embeddingModel.embedDocuments(
+        documents.map((doc) => doc.pageContent),
+      );
+
+      console.log(embeddings[0]);
     }),
 
   getDocumentMessages: protectedProcedure
